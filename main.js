122efa2385ea6e25354b13e24a8d3fb6c34accf7
MasterOfNone963 <arshianaserisenator1385@gmail.com>
2026-09-16 00:00:53 +0330

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ import {
 import { take, tap, catchError } from 'rxjs/operators';
 import { createLogger } from '@veksa/logger';
 
+window.__mainStarted = true;
+
 // ---------- DOM refs ----------
 const symbolSelect = document.getElementById('symbolSelect');
 const bidPriceEl = document.getElementById('bidPrice');
